ecf1dc184c32d57454b897cf1123f71c83004047
katarzyna <katarzynasycz@gmail.com>
2018-05-13 13:41:25 +0200

    Removed old folder

--- Weight_Converter/js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('output').style.visibility = 'hidden';
-        document.getElementById('weightInput').addEventListener('input', function(e){
-            document.getElementById('output').style.visibility = 'visible';
-            let kilograms = e.target.value;
-            document.getElementById('gramsOutput').innerHTML = kilograms*1000;
-            document.getElementById('poundsOutput').innerHTML = kilograms/0.45359237;
-            document.getElementById('ouncesOutput').innerHTML = kilograms/0.02834952;
-        });
