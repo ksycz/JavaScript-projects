0f7c8d27b2918cbc3dd7beeab00205e631b7606d
katarzyna <katarzynasycz@gmail.com>
2018-05-13 16:56:31 +0200

    Add the side navigation and slideshow gallery projects

---
+++ web_elements/side-navigation/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mainSideNav").style.width = "300px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+function closeNav() {
+    document.getElementById("mainSideNav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
