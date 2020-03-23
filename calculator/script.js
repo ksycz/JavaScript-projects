6d8672be4bda63443b97c95d76bbf8c6b83bf12c
Katarzyna Sycz <katarzynasycz@gmail.com>
2020-03-23 23:10:34 +0100

    added possibility of using decimals

--- calculator/script.js
+++ calculator/script.js
@@ -61,8 +61,22 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        const floatNumber = parseFloat(number);
-        return number
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+            maximumFractionDigits: 0 })
+        }
+        if(decimalDigits !=null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     }
 
     updateDisplay() {
