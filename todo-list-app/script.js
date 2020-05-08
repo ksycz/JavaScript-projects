87fc0bf9012505dc3ddb380c863ff006e85f55b7
Katarzyna Sycz <katarzynasycz@gmail.com>
2020-05-08 16:11:50 +0200

    finished JS for todo-list app

---
+++ todo-list-app/script.js
@@ -0,0 +1,147 @@
+const listsContainer = document.querySelector("[data-lists]");
+const newListForm = document.querySelector("[data-new-list-form]");
+const newListInput = document.querySelector("[data-new-list-input]");
+const deleteListButton = document.querySelector("[data-delete-list-button]");
+const listDisplayContainer = document.querySelector("[data-list-display-container");
+const listTitle = document.querySelector("[data-list-title]");
+const listCount = document.querySelector("[data-list-count]");
+const tasksContainer = document.querySelector("[data-tasks]");
+const taskTemplate = document.getElementById("task-template");
+const newTaskForm = document.querySelector("[data-new-task-form]");
+const newTaskInput = document.querySelector("[data-new-task-input]");
+const clearCompleteTasksButton = document.querySelector("[data-clear-complete-tasks-button]");
+
+// creating namespace
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
+// if it exists parse it into object, if not get empty array
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listsContainer.addEventListener("click", e => {
+    if(e.target.tagName.toLowerCase() === "li") {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+});
+
+tasksContainer.addEventListener("click", e => {
+    if(e.target.tagName.toLowerCase() === "input") {
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
+        selectedTask.complete = e.target.checked;
+        save();
+        renderTaskCount(selectedList);
+    }
+});
+
+clearCompleteTasksButton.addEventListener("click", e => {
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
+    saveAndRender();
+});
+
+deleteListButton.addEventListener("click", e => {
+    lists = lists.filter(list => list.id !== selectedListId);
+    selectedListId = null;
+    saveAndRender();
+});
+
+newListForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName == '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    saveAndRender();
+});
+
+newTaskForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const taskName = newTaskInput.value;
+    if (taskName == null || taskName == '') return;
+    const task = createTask(taskName);
+    newTaskInput.value = null;
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks.push(task);
+    saveAndRender();
+});
+
+// as an id we will use the current date
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: []}
+};
+
+function createTask(name) {
+    return { id: Date.now().toString(), name: name, complete: false}
+};
+
+// saving lists in local storage
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+// render li for the list item
+function render() {
+    clearElement(listsContainer);
+    renderLists();
+    const selectedList = lists.find(list => list.id === selectedListId);
+
+    if(selectedListId == null) {
+        listDisplayContainer.style.display = "none";
+    } else {
+        listDisplayContainer.style.display = "";
+        listTitle.innerText = selectedList.name;
+        renderTaskCount(selectedList);
+        clearElement(tasksContainer);
+        renderTasks(selectedList);
+    }
+};
+
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+        // true is necessary to render the full template
+        const taskElement = document.importNode(taskTemplate.content, true);
+        const checkbox = taskElement.querySelector("input");
+        checkbox.id = task.id;
+        checkbox.checked = task.complete;
+        const label = taskElement.querySelector("label");
+        label.htmlFor = task.id;
+        label.append(task.name);
+        tasksContainer.appendChild(taskElement);
+    })
+};
+
+function renderTaskCount(selectedList) {
+    const incompleteTasksCount = selectedList.tasks.filter(task => !task.complete).length;
+    const taskString = incompleteTasksCount === 1 ? "task" : "tasks";
+    listCount.innerText = `${incompleteTasksCount} ${taskString} remaining`;
+}
+
+function renderLists() {
+    lists.forEach(list => {
+        const listElement = document.createElement("li");
+        listElement.dataset.listId = list.id;
+        listElement.classList.add("list-name");
+        listElement.innerText = list.name;
+        if(list.id === selectedListId) {
+            listElement.classList.add("active-list");
+        }
+        listsContainer.appendChild(listElement);
+    })
+};
+
+function clearElement(el) {
+    while (el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+};
+
+render();
